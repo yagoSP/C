858d5b174b556822db5210aa73232b76a4d04283
yagoSP <54825415+yagoSP@users.noreply.github.com>
2021-11-13 15:41:03 -0300

    Create delta.c

---
+++ delta.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int a, b, c, delta;
+    printf("Calculador de Delta\nFormula: Delta = b^2-4*a*c\n");
+    //input de valores
+    printf("Coloque o valor de [b]:\t");
+    scanf("%d", &b);
+    printf("Coloque o valor de [a]:\t");
+    scanf("%d", &a);
+    printf("Coloque o valor de [c]:\t");
+    scanf("%d", &c);
+    //matemática e printf
+    delta = (pow(b,2))-4*(a*c);
+    printf("%d", delta);
+    //end 
+    return 0;
+}
