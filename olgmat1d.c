6246ac8297cc4a9a46814f0ad55930116b36b89d
yagoSP <54825415+yagoSP@users.noreply.github.com>
2021-11-22 21:40:56 -0300

    Create olgmat1d.c
    Resolução de um exercício de matemática.
    Olga [1°D] 
    22/11/2021

---
+++ olgmat1d.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define pmc 0.10
+#define jrc 0.16
+int main(){
+  int mes, f;
+  float si, sap, sf, jr, pm;
+  printf("Mes:\t");
+  scanf("%d", &mes);
+  printf("Saldo Inicial:\t");
+  scanf("%f", &si);
+  for(f=1; f<=mes; f++){
+    pm = si * pmc;
+    sap = si - pm;
+    jr = sap * jrc;
+    sf = sap + jr;
+    printf("%d\t %f, %f, %f, %f, %f,", f, si, pm, sap, jr, sf);
+  }
+  return 0;
+}
+//Criado por YagoSP 22/11/2021 
+//21:40
