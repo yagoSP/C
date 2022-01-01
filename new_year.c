4460309be71f6acc2827f33075daa270b768218d
Yago <54825415+yagoSP@users.noreply.github.com>
2021-12-31 21:31:09 -0300

    Create new_year.c

---
+++ new_year.c
@@ -0,0 +1,17 @@
+//Programa que imprime quantas vezes você quiser a mensagem "Feliz Ano Novo!"
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main(){
+  int year, i;
+  setlocale(LC_ALL, "Portuguese");
+  printf("Digite qual ano está acabando:\t");
+  scanf("%d", &year);
+  for(i=0; i<=year; i++){
+    printf("[%d] Adeus %d!", i, year);
+  }
+  return 0;
+}
+/* Adeus 2021! 
+Criado por YagoSP // 31/12/2021 21:31
+*/
