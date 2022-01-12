e3124a6662bec50a662da1388b957fe94d392de3
Yago <54825415+yagoSP@users.noreply.github.com>
2022-01-11 21:55:32 -0300

    Create vetorNome.c

---
+++ vetorNome.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void){
+    int blc;
+    char met[10][50] = {
+        "Chorelis",
+        "Thiago",
+        "Samuel",
+        "Jack",
+        "James"
+    };
+    printf("Digite o valor do bloco:\t");
+    scanf("%d", &blc);
+    printf("%s\n", met[blc]);
+    return 0;
+}
+/*
+(Linha 5) {O primeiro valor entre colchetes é o tamanho da string.
+O segundo valor entre colchetes é a quantidade de "blocos" que o vetor tem.}
+[Criado por YagoSP 11/01/2022 (21:55)]
