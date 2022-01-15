b5370dd7f594f127fb91c8c4a1ace1e46ab8237d
Yago <54825415+yagoSP@users.noreply.github.com>
2022-01-14 23:55:25 -0300

    Create metallica.c

---
+++ metallica.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(void){
+    char ifse;
+    char met[20][10] = {
+        "Cliff",
+        "James",
+        "Jason",
+        "Trujillo",
+        "Lars",
+        "Kirk",
+        
+    };
+    srand(time(NULL));
+    int i = rand() % 5;
+    
+    printf("Quer descobrir um nome [y] || [n]:\t");
+    scanf("%c", &ifse);
+    if(ifse == 'y' || ifse == 'Y'){
+        
+        printf("Voce seria %s", met[i]);
+        
+    }
+    else{
+        printf("Ate uma outra hora!");
+        return 0;
+    }
+    return 0;
+}
+//Criado por YagoSP 14/01/2022 23:55
