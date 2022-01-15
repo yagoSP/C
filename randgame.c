aeeb10f7243e48efa8e39334c9b8694f9a37262a
Yago <54825415+yagoSP@users.noreply.github.com>
2022-01-15 01:05:34 -0300

    Update randgame.c

--- randgame.c
+++ randgame.c
@@ -3,19 +3,19 @@
 #include <stdlib.h>
 #include <time.h>
 void rando(){
-    int score, random, ask, qnt, count; //Declaração de variáveis;
+    int score, randomi, ask, qnt, count; //Declaração de variáveis;
     srand(time(NULL));
-    random = 10; //Valor simulando o rand();.
+    randomi = 10; //Valor simulando o rand();.
     score = 0; // Valor inicial do score.
     printf("Quantas vezes quer jogar:\t");
     scanf("%d", &qnt);
     for(count=1; count<=qnt; count++){ //For para a quantidade de jogadas.
         printf(">_:\t");
         scanf("%d", &ask);
-        if(ask == random){
+        if(ask == randomi){
             score++;
         }
-        if(ask != random){ //if para a perca de pontos.
+        if(ask != randomi){ //if para a perca de pontos.
             score = score -1;
         }
     }
