c492c9e0744c2290b5cbdbe1ede2909665fc846e
Yago <54825415+yagoSP@users.noreply.github.com>
2023-01-03 01:12:47 -0300

    Update japan.c

--- japan.c
+++ japan.c
@@ -1,26 +1,23 @@
-/* this code was writed by YagoSP [02:17 || 30/12/2022] */
+/* this code was writed by YagoSP [02:17 || 30/12/2022] 
+                 Edited by YagoSP [01:12 || 03/01/2023]*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <locate.h>
-#include <wchar.h>
-#include <time.h>
 #include <unistd.h>
 int main(void){
     char hiragana[64][64]={
-        "ぁ","ぃ","ぅ","ぇ","ぉ",
-        "か","き","く","け","こ",
-        "さ","し","す","せ","そ",
-        "た","ち","っ","て","と",
-        "な","に","ぬ","ね","の",
-        "は","ひ","ふ","へ","ほ",
-        "ま","み","む","め","も",
-        "ゃ","ゅ","ょ",
-        "ら","り","る","れ","ろ",
-        "わ","を","ん"
+        "ぁ= A","ぃ= I","ぅ= U","ぇ= E","ぉ= O",
+	    "か= KA","き= KI","く= KU","け= KE","こ= KO",
+	    "さ= SA","し= SHI","す= SU","せ= SE","そ= SO",
+	    "た= TA","ち= CHI","っ= TSU","て= TE","と= TO",
+	    "な= NA","に= NI","ぬ= NU","ね= NE","の= NO",
+	    "は= HA","ひ= HI","ふ= FU","へ= HE","ほ= HO",
+	    "ま= MA","み= MI","む= MU","め= ME","も= MO",
+	    "ゃ= YA","ゅ= YU","ょ= YO",
+	    "ら= RA","り= RI","る= RU","れ= RE","ろ = RO",
+	    "わ= WA","を= o","ん= N"
         };
-    srand(time(NULL));
-    while(1){
-        int i = rand() % 46;
+    int i;
+    for(i=0; i<=46; i++){
         printf("%s\n", hiragana[i]);
         sleep(2);
     };
