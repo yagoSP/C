257e865532c7a60b08b9b7abb2b50f89017d013f
Yago <54825415+yagoSP@users.noreply.github.com>
2024-08-24 17:03:36 -0300

    Add files via upload

---
+++ logica_senac/somaABC.c
@@ -0,0 +1,38 @@
+/*
+ *  Faça um algoritmo que leia os valores de A, B C 	     
+ *  e em seguida imprima na tela a soma entre A e B e mostre 
+ *  se a soma é menor que C.                                 
+ */
+
+// Includes
+#include <stdio.h>
+
+int main(void){
+	
+	// Declaração de variáveis
+	int a, b, c, resultSum;
+	
+	// Inserção de valores
+	
+	printf("Digite o valor de A: ");
+	scanf("%d", &a);
+
+	printf("Digite o valor de B: ");
+	scanf("%d", &b);
+
+	printf("Digite o valor de C: ");
+	scanf("%d", &c);
+
+	// Parte Lógica
+	
+	resultSum = a + b;
+	if(resultSum > c){
+		printf("%d e maior que %d!\n", resultSum, c);
+	}
+	else{
+		printf("%d e menor que %d!\n", resultSum, c);
+	}
+	
+	// Final
+	return 0;
+}
